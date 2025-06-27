b31c29389ffeee1b178a23f1277bbf513b18bad8
Ankit <ankitmittal0814@gmail.com>
2025-06-27 12:59:41 +0530

    Fix redis key usage in string.js for correct data retrieval

--- string.js
+++ string.js
@@ -1,9 +1,9 @@
 const client = require('./client.js');
 
 async function init() {
-    // await client.set('user:4', 'Redis');
-    await client.expire('user:4', 20);
-    const res = await client.get('user:4');
+    await client.set('user:1', 'Redis');
+    // await client.expire('user:4', 20);
+    const res = await client.get('user:2');
     console.log('Result -> ', res);
 }
 init();
